777372675aed9ede084e388f7abe052118ca3047
Carlos Santiago <emci1291@gmail.com>
2023-05-05 06:37:02 -0700

    Mod: Se agrgegan consntantes las claves de validacion

--- src/app/Http/Controllers/CodigosPostalesController.php
+++ src/app/Http/Controllers/CodigosPostalesController.php
@@ -14,16 +14,17 @@ class CodigosPostalesController extends Controller
 
     const SEP0MEX_INPUT_FILE = 'sepomex_txt';
 
-    public function getCodigoPostal(Request $request, string $codigoPostal = '') {
+    public function getCodigoPostal(Request $request, string $codigoPostal = '')
+    {
 
 
         $codigosPostaes = [];
-        if(!empty($codigoPostal)) {
+        if (!empty($codigoPostal)) {
             $codigosPostaes = CodigoPostalConverter::where('d_codigo', $codigoPostal);
-                #->where('d_asenta', 'LIKE', "%%")
+            #->where('d_asenta', 'LIKE', "%%")
         }
-        if(!empty($request->get('colonia'))) {
-            $codigosPostaes->where('d_asenta', 'LIKE', '%'.$request->get('colonia').'%');
+        if (!empty($request->get('colonia'))) {
+            $codigosPostaes->where('d_asenta', 'LIKE', '%' . $request->get('colonia') . '%');
         }
         return $codigosPostaes->get();
     }
@@ -32,7 +33,7 @@ class CodigosPostalesController extends Controller
     public function updagradeCodigosPostalesTxtFile()
     {
         // Verifica si el archivo exist que contiene los CP
-        if(Storage::disk('local')->exists('CPdescarga.txt')) {
+        if (Storage::disk('local')->exists('CPdescarga.txt')) {
             Storage::disk()->delete('CPdescarga.txt');
         }
 
@@ -48,13 +49,13 @@ class CodigosPostalesController extends Controller
             'Sec-Fetch-Site'            => 'same-origin',
             'Sec-Fetch-User'            => '?1',
             'Upgrade-Insecure-Requests' => '1'
-        ])->asForm()->post('https://www.correosdemexico.gob.mx/SSLServicios/ConsultaCP/CodigoPostal_Exportar.aspx', [
+        ])->asForm()->post(Constans::SEPOMEX_URL_SERVICE_EXPORTAR, [
             '__EVENTTARGET'        => '',
             '__EVENTARGUMENT'      => '',
             '__LASTFOCUS'          => '',
-            '__VIEWSTATE'          => '/wEPDwUINzcwOTQyOTgPZBYCAgEPZBYCAgEPZBYGAgMPDxYCHgRUZXh0BTfDmmx0aW1hIEFjdHVhbGl6YWNpw7NuIGRlIEluZm9ybWFjacOzbjogQWJyaWwgMyBkZSAyMDIzZGQCBw8QDxYGHg1EYXRhVGV4dEZpZWxkBQNFZG8eDkRhdGFWYWx1ZUZpZWxkBQVJZEVkbx4LXyFEYXRhQm91bmRnZBAVISMtLS0tLS0tLS0tIFQgIG8gIGQgIG8gIHMgLS0tLS0tLS0tLQ5BZ3Vhc2NhbGllbnRlcw9CYWphIENhbGlmb3JuaWETQmFqYSBDYWxpZm9ybmlhIFN1cghDYW1wZWNoZRRDb2FodWlsYSBkZSBaYXJhZ296YQZDb2xpbWEHQ2hpYXBhcwlDaGlodWFodWERQ2l1ZGFkIGRlIE3DqXhpY28HRHVyYW5nbwpHdWFuYWp1YXRvCEd1ZXJyZXJvB0hpZGFsZ28HSmFsaXNjbwdNw6l4aWNvFE1pY2hvYWPDoW4gZGUgT2NhbXBvB01vcmVsb3MHTmF5YXJpdAtOdWV2byBMZcOzbgZPYXhhY2EGUHVlYmxhClF1ZXLDqXRhcm8MUXVpbnRhbmEgUm9vEFNhbiBMdWlzIFBvdG9zw60HU2luYWxvYQZTb25vcmEHVGFiYXNjbwpUYW1hdWxpcGFzCFRsYXhjYWxhH1ZlcmFjcnV6IGRlIElnbmFjaW8gZGUgbGEgTGxhdmUIWXVjYXTDoW4JWmFjYXRlY2FzFSECMDACMDECMDICMDMCMDQCMDUCMDYCMDcCMDgCMDkCMTACMTECMTICMTMCMTQCMTUCMTYCMTcCMTgCMTkCMjACMjECMjICMjMCMjQCMjUCMjYCMjcCMjgCMjkCMzACMzECMzIUKwMhZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZGQCHQ88KwALAGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFC2J0bkRlc2NhcmdhECVy01G7PF2Dj0L6Rb29d7m53oc=',
+            '__VIEWSTATE'          => Constans::TOKEN_STATE_VALUE,
             '__VIEWSTATEGENERATOR' => 'BE1A6D2E',
-            '__EVENTVALIDATION'    => '/wEWKALe/9K6BwLG/OLvBgLWk4iCCgLWk4SCCgLWk4CCCgLWk7yCCgLWk7iCCgLWk7SCCgLWk7CCCgLWk6yCCgLWk+iBCgLWk+SBCgLJk4iCCgLJk4SCCgLJk4CCCgLJk7yCCgLJk7iCCgLJk7SCCgLJk7CCCgLJk6yCCgLJk+iBCgLJk+SBCgLIk4iCCgLIk4SCCgLIk4CCCgLIk7yCCgLIk7iCCgLIk7SCCgLIk7CCCgLIk6yCCgLIk+iBCgLIk+SBCgLLk4iCCgLLk4SCCgLLk4CCCgLL+uTWBALa4Za4AgK+qOyRAQLI56b6CwL1/KjtBb/J+uVF4o+PCuWvMdAnNmu7yUmw',
+            '__EVENTVALIDATION'    => Constans::TOKEN_EVENT_VALIDATION,
             'cboEdo'               => '00',
             'rblTipo'              => 'txt',
             'btnDescarga.x'        => '55',
@@ -72,7 +73,7 @@ class CodigosPostalesController extends Controller
 
         // Descomprimimos el archivo, en dado caso que marque error lo elimina y termina el proceso
         $zip = new \ZipArchive();
-        if(!$zip->open($fileAbsulutePath)) {
+        if (!$zip->open($fileAbsulutePath)) {
             Storage::disk()->delete($fileName);
             throw new \Exception("Error al descargar el archivo desde SEPOMEX");
         }
@@ -85,7 +86,7 @@ class CodigosPostalesController extends Controller
         $lines = 0;
         while (($line = fgets($fileCodigosPostalesContent)) !== false) {
             $lines++;
-            if ($lines < 3){
+            if ($lines < 3) {
                 continue;
             }
             $fields = explode('|', $line);
@@ -112,7 +113,5 @@ class CodigosPostalesController extends Controller
             'file' => basename($fileAbsulutePath),
             'rowsTotal' => CodigoPostalConverter::count(),
         ];
-
     }
-
 }
